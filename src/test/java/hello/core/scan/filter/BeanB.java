254fb6deba1c360d37ca4c6b880029592a4d8605
drum-grammer <drumgrammer@gmail.com>
2022-04-07 17:19:51 +0900

    [SECTION-6]  filter scan

---
+++ src/test/java/hello/core/scan/filter/BeanB.java
@@ -0,0 +1,5 @@
+package hello.core.scan.filter;
+
+@MyExcludeCoomponent
+public class BeanB {
+}
---
+++ src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -0,0 +1,32 @@
+package hello.core.scan.filter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.*;
+
+import static org.springframework.context.annotation.ComponentScan.*;
+
+public class ComponentFilterAppConfigTest {
+
+    @Test
+    void filterScan() {
+
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
+        BeanA beanA = ac.getBean("beanA", BeanA.class);
+        Assertions.assertThat(beanA).isNotNull();
+
+        org.junit.jupiter.api.Assertions.assertThrows(
+                NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("beanB", BeanB.class)
+        );
+    }
+
+    @Configuration
+    @ComponentScan(
+            includeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyIncludeCoomponent.class),
+            excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyExcludeCoomponent.class)
+    )
+    static class ComponentFilterAppConfig {
+    }
+}
