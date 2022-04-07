3b220d97e7061051e74f112fa4c73fd620c31e51
drum-grammer <48014144+drum-grammer@users.noreply.github.com>
2022-04-07 14:03:01 +0900

    [SECTION-5] singletone (#4)

---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,32 @@
+package hello.core.singleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        statefulService1.order("nameA", 1000);
+        statefulService2.order("nameB", 3000);
+
+        int price = statefulService1.getPrice();
+        System.out.println("price = " + price);
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+}
